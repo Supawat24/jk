b6b7d232ce8e424d9241d4b8fccd7ebd64a541a9
Supawat24 <152580123+Supawat24@users.noreply.github.com>
2024-01-19 10:46:52 +0700

    5

---
+++ introphp/index.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+  </head>
+  <body>
+    <h1 class="text-center text-primary"><?PHP echo "เเนะนำตัว"?></h1>
+    <h2 class="text-center text-info"><?PHP echo "นาย ศุภวัฒน์ จรัสสัน 5/6 เลขที่ 15"?></h2>
+    <h2 class="text-center text-warning"><?PHP echo "ชอบกินผลไม้ ชอบเล่นกีฬา ชอบเล่นดนตรี"?></h2>
+    <h2 class="text-center text-danger"><?PHP echo "สเปกคือ คนที่อยู่ด้วยเเล้วสบายใจ ไม่งี่เง่าเอาเเต่ใจ"?></h2>
+
+  
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+  </body>
+</html>
